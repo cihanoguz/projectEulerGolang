31bac772308851fc466e02980c67e86170967840
purelogic01 <44440907+purelogic01@users.noreply.github.com>
2018-10-28 19:05:44 +0300

    Update projectEulerProblem3.go

--- projectEulerProblem3.go
+++ projectEulerProblem3.go
@@ -5,46 +5,67 @@ The prime factors of 13195 are 5, 7, 13 and 29.
 What is the largest prime factor of the number 600851475143 ?
 */
 
+/*  this code works on int type input. I couldnt find how can i change for loop iteration in Golang. Because the number is int64
+
+*/
+
+
 package main
 
 import "fmt"
 
 import "math"
+func isPrime(value int) bool {
+    for i := 2; i <= int(math.Sqrt(float64(value)/2)); i++ {
+        if value%i == 0 {
+            return false
+        }
+    }
+    return value > 1
+}
+/*func pressPrime(value int)int{
+	var prime int
+	prime = 1
+	if isPrime(value) == true{
+		prime = value;
 
-func largestPrimeFactor(n int) int{
+	}
+	return prime
+}
+*/
+
+func largestPrimeFactor(n int)int{
 	var largestPrimeFact int
-	largestPrimeFact = 0
+	largestPrimeFact = n
 	var t float64
+
+	
 	t = float64(n)
 	var p int = int(math.Sqrt(t))
 	for i := 2; i <= p; i++ { 
-        if (n % i == 0) { 
-            largestPrimeFact = i; 
-		} 
-	} 
-	var k int
-	k = n / largestPrimeFact
-	var l float64
-	l = float64(k)
-	for i := 2; i <= int(math.Sqrt(l)); i++{
-		if int(k) % i !=0 {
-			largestPrimeFact=k
+		if isPrime(i)== true {
+			for n%i == 0 {
+				n=n/i;
+				largestPrimeFact = i
+			}
+		
 		}
-
-    }
-
-	
-	
+		if n > largestPrimeFact {
+			largestPrimeFact=n
+		}	
+	}
 	return largestPrimeFact
 
 
 }
+
+
+
 func main(){
-	fmt.Println("enter a number:")
-	var n int
-	fmt.Scan(&n)
-	fmt.Println(largestPrimeFactor(n))
-
-
+	
+		fmt.Println("enter a number:")
+		var n int
+		fmt.Scan(&n)
+		fmt.Println(largestPrimeFactor(n))
 
 }
